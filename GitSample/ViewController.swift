5b9772e401d0a3e84da458ed6cf0309074f96820
Jina <cherub709@gmail.com>
2017-09-18 21:43:41 +0900

    commit test

---
+++ GitSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitSample
+//
+//  Created by Jina on 2017. 9. 18..
+//  Copyright © 2017년 Jina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //HEllo Jina   
+        print("aaa")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
